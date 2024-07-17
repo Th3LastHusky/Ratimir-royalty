49e21506072e063e74f9b0e2a9b45b1bf525ba58
Th3LastHusky <galibindmitry@gmail.com>
2024-07-17 17:36:29 +0300

    Initial commit

---
+++ lib/config/plugin.php
@@ -0,0 +1,10 @@
+<?php
+return array (
+  'name' => 'Ratimirroyalty',
+  'img' => 'img/ratimirroyalty.gif',
+  'version' => '0.0.1',
+  'vendor' => '--',
+  'handlers' => 
+  array (
+  ),
+);
---
+++ lib/shopRatimirroyaltyPlugin.class.php
@@ -0,0 +1,6 @@
+<?php
+
+class shopRatimirroyaltyPlugin extends shopPlugin
+{
+
+}
